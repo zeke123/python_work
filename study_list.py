0b382e9bb9f508b5d0815b1af8e44b4cc7db3588
zeke123 <zekezhoujian@126.com>
2025-04-02 15:58:57 +0800

    学习Python中的列表list

--- study_list.py
+++ study_list.py
@@ -20,5 +20,24 @@ print(languages)
 languages.append('Swift')
 print(languages)
 
-#
+# 在列表中插入数据,在列表的第二个位置上，插入数据
+languages.insert(1,'C++')
+print(languages)
 
+# 从列表中删除元素,使用del语句删除
+del languages[0]
+print(languages)
+
+# 使用pop()方法删除列表末尾元素
+print(languages.pop())  # Swift
+print(languages)
+
+
+# 删除列表中任意位置的元素
+del_language = languages.pop(0)
+print(del_language)
+print(languages)
+
+# 根据值删除元素
+languages.remove('ArkTS')
+print(languages)
