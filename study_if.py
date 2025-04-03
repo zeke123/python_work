fc827ef47e8399e3a50a80d97870ec03b47f5895
zeke123 <zekezhoujian@126.com>
2025-04-03 10:59:38 +0800

    Python中if语句学习

---
+++ study_if.py
@@ -0,0 +1,34 @@
+# 一个简单的示例
+cars = ['bmw','toyota','audi','subaru']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+
+car = 'bmw'
+print(car == 'bmw') # True
+print(car == 'audi') # False
+
+# 不等运算符 !=
+
+# 检查多个条件
+# and：每个条件都通过了，整个表达式为True，如果至少一个条件没通过，就为False
+# or：其中一个条件通过了，整个表达式为True，否则为False
+
+age_student_A = 25
+age_student_B = 28
+
+print("------1")
+
+if age_student_A >=25 and age_student_B >=28:
+    print(True)
+else:
+    print(False)
+print("------2")
+
+if age_student_A >=26 or age_student_B >=30:
+    print(True)
+else:
+    print(False)
