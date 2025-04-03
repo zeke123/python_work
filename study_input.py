edf473363f4496cf63bf7576546ec106da74ac1c
zeke123 <zekezhoujian@126.com>
2025-04-03 22:34:40 +0800

    Python中input学习

--- study_input.py
+++ study_input.py
@@ -1,5 +1,19 @@
 """
 input函数:让程序暂停运行,等待用户输入一些文本。获取用户输入后,Python将其赋给一个变量，以便使用’
 """
-message = input('请输入相关信息：')
-print(message)
+#message = input('请输入相关信息：')
+#print(message)
+
+
+#name = input('please enter your name:')
+#print(f"\nHello,{name}")
+
+
+# int():输入的
+
+age = input('你今年多大了：')
+print(age)
+if int(age)>=18:
+    print("成年人")
+else:
+    print("未成年人")
